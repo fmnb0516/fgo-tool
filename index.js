30107c511f6ff1dd90379d279e52670cedc1f4cd
fmnb0516 <fmnb0516@mnb.bz>
2019-09-12 17:21:48 +0900

    update.

--- index.js
+++ index.js
@@ -29,11 +29,42 @@ app.get('/api/v1/:id',function(req,res){
 });
 
 app.put('/api/v1/:id',function(req,res){
-    res.json({});
+    var id = req.params.id;
+    var json = req.body;
+    fs.writeFile(publicDir + "/app/" + id, JSON.stringify(json, null , "\t"), 'utf-8', (err, data) => {
+        res.json({"message":"更新しました"});
+    });
 });
 
+const clazzTo = (function() {
+
+    const data = {
+        "saber" : "剣",
+        "archer" : "弓",
+        "luncer" : "槍",
+        "rider" : "騎",
+        "caster" : "術",
+        "asasin" : "殺",
+        "barserker" : "狂",
+        "ruler" : "裁",
+        "avenger" : "讐",
+        "monncanser" : "月",
+        "alterego" : "分",
+        "foreigner" : "降",
+    };
+
+    return function(c) {
+        return data[c];
+    };
+})();
+
 app.post('/api/v1/create',function(req,res){
-    res.json({});
+    var json = req.body;
+    var id =　json.servant.no + "-" + clazzTo(json.servant.clazz) + "-" + json.servant.rare + "-" + json.servant.name + ".json";
+    
+    fs.writeFile(publicDir + "/app/" + id, JSON.stringify(json, null , "\t"), 'utf-8', (err, data) => {
+        res.json({"message":"作成しました"});
+    });
 });
 
 app.delete('/api/v1/:id',function(req,res){
