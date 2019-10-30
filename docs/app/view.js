0b4c6bcc0dd8057507eee26de01341745f3316ff
fmnb0516 <fmnb0516@mnb.bz>
2019-10-30 15:22:32 +0900

    update.

--- docs/app/view.js
+++ docs/app/view.js
@@ -2,11 +2,37 @@
 (function() {
     var result = {};
 
+    function getServantData(no) {
+        var data = fgo.data().find(function (d) {
+            return d.servant.no === no;
+        });
+        return data ? data : null;
+    };
+
+    var constData = fgo.constData();
+
     result.load = function() {
+        
+
+        result.template = Handlebars.compile($("#template-view-page").html());
+
+        Handlebars.registerHelper('fgo-buf-type-option', function (v1) {
+            var html = constData.effects.map(function (t) {
+              var selected = t === v1 ? "selected" : "";
+              return "<option " + selected + " value=\"" + t + "\">" + t + "</option>";
+            });
+            return new Handlebars.SafeString(html);
+        });
     };
 
     result.rerender = function(query) {
-        return "";
+        var no = query.split("-")[0];
+        var data = getServantData(no);
+
+        return result.template({
+            data : data,
+            constData: constData,
+        });
     };
 
     result.rendered = function(query, container) {
