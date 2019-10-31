306711c9f84af879f73e3abe93dbc31aff91c9a6
fmnb0516 <fmnb0516@mnb.bz>
2019-10-31 16:56:07 +0900

    update.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static(publicDir));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const run = async () => {
+const servantdata_gen = async () => {
     const readdir = util.promisify(fs.readdir);
     const readFile = util.promisify(fs.readFile);
     const writeFile = util.promisify(fs.writeFile);
@@ -25,7 +25,7 @@ const run = async () => {
 
     for (let i = 0; i < files.length; i++) {
         const f = files[i];
-        const data = await readFile(publicDir + "/json/" + f);
+        const data = await readFile(publicDir + "/json/" + f, "utf8");
         allData.push(JSON.parse(data));
     }
 
@@ -33,7 +33,45 @@ const run = async () => {
     await writeFile(publicDir + "/servant.js", script, "utf8");
 };
 
-run().then(() => {
+const articledata_gen = async () => {
+    const allData = [];
+
+    const readdir = util.promisify(fs.readdir);
+    const readFile = util.promisify(fs.readFile);
+    const writeFile = util.promisify(fs.writeFile);
+
+    const files = (await readdir(publicDir + "/article")).filter(f => f.endsWith(".md"));
+    for (let i = 0; i < files.length; i++) {
+        const f = files[i];
+        const data = await readFile(publicDir + "/article/" + f, "utf8");
+
+        const lines = data.match(/<!--([\s\S]*?)-->/g)[0].replace("<!--","").replace("-->","").trim().split("\r\n");
+
+        const metadata = {};
+        
+        lines.forEach((line) => {
+            const idx = line.indexOf(":");
+            const key = line.substring(0, idx);
+            const val = line.substring(idx+1).trim();
+            metadata[key] = val;
+        });
+
+        const url = "article/"+f;
+
+        allData.push({
+            url : url,
+            metadata : metadata
+        });
+    }
+
+    const script = "var ARTICLE_ENTRIES = " + JSON.stringify(allData, null, "  ") + ";"; 
+    await writeFile(publicDir + "/articles.js", script, "utf8");
+};
+
+const p1 = servantdata_gen();
+const p2 = articledata_gen();
+
+Promise.all([p1, p2]).then(() => {
     app.listen(port);
     console.log('listen on port ' + port);
 });
